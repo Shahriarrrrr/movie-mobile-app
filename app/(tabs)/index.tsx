0267f58b2416689b33f64426796d6e1bb7a90b1f
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-07-24 18:19:35 +0600

    added Nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">MOOOVIE!</Text>
-      <Link href="/onboarding">Onboarding</Link>
-      <Link className="text-white text-2xl bg-slate-600" href="/movie/avengers">Avenger Movie</Link>
     </View>
 
   );
