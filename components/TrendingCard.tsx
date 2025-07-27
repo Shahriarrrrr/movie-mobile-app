91876cd5aec5d160ceeae5a6b8e3c64428c282bf
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-07-27 19:54:00 +0600

    ProjectDone

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,6 +1,8 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import MaskedView from '@react-native-masked-view/masked-view';
+import { images } from '@/constants/images';
 
 const TrendingCard = ({movie: {movie_id, title, poster_url}, index} : TrendingCardProps) => {
   return (
@@ -13,6 +15,14 @@ const TrendingCard = ({movie: {movie_id, title, poster_url}, index} : TrendingCa
             >
                 
             </Image>
+        <View className='absolute bottom-9 -left-3.5 px-2 py-1 rounded-full'>
+            <MaskedView maskElement={<Text className='font-bold text-white text-6xl'>{index+1}</Text>}>
+            <Image source={images.rankingGradient} className='size-14' resizeMode='cover'></Image>
+            </MaskedView>
+        </View>
+        <Text className='text-sm font-bold mt-2 text-light-200' numberOfLines={2}>
+            {title}
+            </Text>    
         </TouchableOpacity>
     </Link>
   )
--- services/api.ts
+++ services/api.ts
@@ -23,4 +23,23 @@ export const fetchMovies =  async ({query} : {query: string}) => {
     return data.results
 }
 
+export const fetchMovieDetails = async(movieId:string) : Promise<MovieDetails> => {
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,{
+            method: 'GET',
+            headers: TMDB_CONFIG.headers,
+        });
+        if(!response.ok){
+            throw new Error('Failed to fetch a movie details')
+        }
+        const data = await response.json();
+        return data;
+
+
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
